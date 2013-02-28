9c179b54a167dbf729100986367d544287ae6b4f
pyrsmk <dev@dreamysource.fr>
2013-02-28 17:51:37 +0100

    Update unit's ender file

--- unit/ender.js
+++ unit/ender.js
@@ -7,11 +7,11 @@
 
 /*!
   * Ender: open module JavaScript framework (client-lib)
-  * copyright Dustin Diaz & Jacob Thornton 2011 (@ded @fat)
-  * http://ender.no.de
+  * copyright Dustin Diaz & Jacob Thornton 2011-2012 (@ded @fat)
+  * http://ender.jit.su
   * License MIT
   */
-!function (context) {
+(function (context) {
 
   // a global object for node.js module compatiblity
   // ============================================
@@ -23,17 +23,20 @@
   // ============================================
 
   var modules = {}
-    , old = context.$
+    , old = context['$']
+    , oldEnder = context['ender']
+    , oldRequire = context['require']
+    , oldProvide = context['provide']
 
   function require (identifier) {
     // modules can be required from ender's build system, or found on the window
-    var module = modules[identifier] || window[identifier]
-    if (!module) throw new Error("Requested module '" + identifier + "' has not been defined.")
+    var module = modules['$' + identifier] || window[identifier]
+    if (!module) throw new Error("Ender Error: Requested module '" + identifier + "' has not been defined.")
     return module
   }
 
   function provide (name, what) {
-    return (modules[name] = what)
+    return (modules['$' + name] = what)
   }
 
   context['provide'] = provide
@@ -44,319 +47,81 @@
     return o
   }
 
-  function boosh(s, r, els) {
+  /**
+   * main Ender return object
+   * @constructor
+   * @param {Array|Node|string} s a CSS selector or DOM node(s)
+   * @param {Array.|Node} r a root node(s)
+   */
+  function Ender(s, r) {
+    var elements
+      , i
+
+    this.selector = s
     // string || node || nodelist || window
-    if (typeof s == 'string' || s.nodeName || (s.length && 'item' in s) || s == window) {
-      els = ender._select(s, r)
-      els.selector = s
-    } else els = isFinite(s.length) ? s : [s]
-    return aug(els, boosh)
+    if (typeof s == 'undefined') {
+      elements = []
+      this.selector = ''
+    } else if (typeof s == 'string' || s.nodeName || (s.length && 'item' in s) || s == window) {
+      elements = ender._select(s, r)
+    } else {
+      elements = isFinite(s.length) ? s : [s]
+    }
+    this.length = elements.length
+    for (i = this.length; i--;) this[i] = elements[i]
   }
+
+  /**
+   * @param {function(el, i, inst)} fn
+   * @param {Object} opt_scope
+   * @returns {Ender}
+   */
+  Ender.prototype['forEach'] = function (fn, opt_scope) {
+    var i, l
+    // opt out of native forEach so we can intentionally call our own scope
+    // defaulting to the current item and be able to return self
+    for (i = 0, l = this.length; i < l; ++i) i in this && fn.call(opt_scope || this[i], this[i], i, this)
+    // return self for chaining
+    return this
+  }
+
+  Ender.prototype.$ = ender // handy reference to self
+
 
   function ender(s, r) {
-    return boosh(s, r)
+    return new Ender(s, r)
   }
 
-  aug(ender, {
-      _VERSION: '0.3.6'
-    , fn: boosh // for easy compat to jQuery plugins
-    , ender: function (o, chain) {
-        aug(chain ? boosh : ender, o)
-      }
-    , _select: function (s, r) {
-        return (r || document).querySelectorAll(s)
-      }
-  })
+  ender['_VERSION'] = '0.4.3-dev'
 
-  aug(boosh, {
-    forEach: function (fn, scope, i) {
-      // opt out of native forEach so we can intentionally call our own scope
-      // defaulting to the current item and be able to return self
-      for (i = 0, l = this.length; i < l; ++i) i in this && fn.call(scope || this[i], this[i], i, this)
-      // return self for chaining
-      return this
-    },
-    $: ender // handy reference to self
-  })
+  ender.fn = Ender.prototype // for easy compat to jQuery plugins
 
-  ender.noConflict = function () {
-    context.$ = old
+  ender.ender = function (o, chain) {
+    aug(chain ? Ender.prototype : ender, o)
+  }
+
+  ender._select = function (s, r) {
+    if (typeof s == 'string') return (r || document).querySelectorAll(s)
+    if (s.nodeName) return [s]
+    return s
+  }
+
+
+  // use callback to receive Ender's require & provide and remove them from global
+  ender.noConflict = function (callback) {
+    context['$'] = old
+    if (callback) {
+      context['provide'] = oldProvide
+      context['require'] = oldRequire
+      context['ender'] = oldEnder
+      if (typeof callback == 'function') callback(require, provide, this)
+    }
     return this
   }
 
   if (typeof module !== 'undefined' && module.exports) module.exports = ender
   // use subscript notation as extern for Closure compilation
-  context['ender'] = context['$'] = context['ender'] || ender
+  context['ender'] = context['$'] = ender
 
-}(this);
+}(this));
 
-!function () {
-
-  var module = { exports: {} }, exports = module.exports;
-
-  /*
-      nut, the concise CSS selector engine
-  
-      Version     : 0.1.18
-      Author      : Aurélien Delogu (dev@dreamysource.fr)
-      Homepage    : https://github.com/pyrsmk/nut
-      License     : MIT
-  */
-  
-  (function(def){
-      if(typeof module!='undefined'){
-          module.exports=def;
-      }
-      else{
-          this.nut=def;
-      }
-  }(function(){
-      
-      var doc=document,
-          firstChild='firstChild',
-          nextSibling='nextSibling',
-          getElementsByClassName='getElementsByClassName',
-          length='length',
-      
-      /*
-          Get all nodes
-          
-          Parameters
-              string selector : a selector
-              context         : a context
-          
-          Return
-              object          : nodes
-      */
-      getAllNodes=function(selector,context){
-          var node=context[firstChild],
-              nodes=[];
-          // Reduce
-          if(node){
-              do{
-                  if(node.nodeType==1){
-                      nodes.push(node);
-                  }
-              }
-              while(node=node[nextSibling]);
-          }
-          return nodes;
-      },
-      
-      /*
-          Get id node
-          
-          Parameters
-              string selector : a selector
-              context         : a context
-          
-          Return
-              object          : nodes
-      */
-      getNodesFromIdSelector=function(selector,context){
-          var node=doc.getElementById(selector);
-          if(!node){
-              return [];
-          }
-          else{
-              return [node];
-          }
-      },
-      
-      /*
-          Get nodes corresponding to a class name (for IE<9)
-  
-          Parameters
-              string name     : class name
-              object context  : contextual node
-  
-          Return
-              array           : found nodes
-      */
-      getNodesByClassName=function(name,context){
-          // Init vars
-          var node=context[firstChild],
-              nodes=[],
-              elements;
-          // Browse children
-          if(node){
-              do{
-                  if(node.nodeType==1){
-                      // Match the class
-                      if(node.className && node.className.match('\\b'+name+'\\b')){
-                          nodes.push(node);
-                      }
-                      // Get nodes from node's children
-                      if((elements=getNodesByClassName(name,node))[length]){
-                          nodes=nodes.concat(elements);
-                      }
-                  }
-              }
-              while(node=node[nextSibling]);
-          }
-          return nodes;
-      },
-      
-      /*
-          Get nodes from a class selector
-          
-          Parameters
-              string selector : a selector
-              context         : a context
-          
-          Return
-              object          : nodes
-      */
-      getNodesFromClassSelector=function(selector,context){
-          if(context[getElementsByClassName]){
-              return context[getElementsByClassName](selector);
-          }
-          else{
-              return getNodesByClassName(selector,context);
-          }
-      },
-      
-      /*
-          Get nodes from a tag selector
-          
-          Parameters
-              string selector : a selector
-              context         : a context
-          
-          Return
-              object          : nodes
-      */
-      getNodesFromTagSelector=function(selector,context){
-          return context.getElementsByTagName(selector);
-      };
-      
-      /*
-          Select DOM nodes
-  
-          Parameters
-              string selectors        : CSS selectors
-              array, object contexts  : contextual nodes
-  
-          Return
-              array                   : found nodes
-      */
-      return function(selectors,contexts){
-          // Format contexts
-          if(!contexts){
-              contexts=[doc];
-          }
-          else if(!contexts.pop){
-              contexts=[contexts];
-          }
-          // Init vars
-          var context,
-              local_contexts,
-              future_local_contexts,
-              selector,
-              elements,
-              nodes=[],
-              i=-1,
-              j,k,l,m,n,o,
-              getNodesFromSelector;
-          // Prepare selectors
-          selectors=selectors.split(',');
-          n=-1;
-          while(selector=selectors[++n]){
-              selectors[n]=selector.split(/\s+/);
-          }
-          // Evaluate selectors for each global context
-          while(context=contexts[++i]){
-              j=selectors[length];
-              while(j){
-                  // Init local context
-                  local_contexts=[context];
-                  // Evaluate selectors
-                  k=-1;
-                  l=selectors[--j][length];
-                  while(++k<l){
-                      // Drop empty selectors
-                      if(selector=selectors[j][k]){
-                          // Id
-                          if(selector.charAt(0)=='#'){
-                              selector=selector.substr(1);
-                              getNodesFromSelector=getNodesFromIdSelector;
-                          }
-                          // Class
-                          else if(selector.charAt(0)=='.'){
-                              selector=selector.substr(1);
-                              getNodesFromSelector=getNodesFromClassSelector;
-                          }
-                          // Joker
-                          else if(selector=='*'){
-                              getNodesFromSelector=getAllNodes;
-                          }
-                          // Tag
-                          else{
-                              getNodesFromSelector=getNodesFromTagSelector;
-                          }
-                          // Evaluate current selector for each local context
-                          future_local_contexts=[];
-                          m=local_contexts[length];
-                          while(m){
-                              elements=getNodesFromSelector(selector,local_contexts[--m]);
-                              n=-1;
-                              o=elements[length];
-                              while(++n<o){
-                                  future_local_contexts.push(elements[n]);
-                              }
-                          }
-                          // Set new local contexts
-                          local_contexts=future_local_contexts;
-                      }
-                  }
-                  // Append new nodes
-                  nodes=nodes.concat(local_contexts);
-              }
-          }
-          return nodes;
-      };
-  
-  }()));
-
-  provide("nut", module.exports);
-
-  // inspired from https://github.com/ded/qwery/blob/master/src/ender.js
-  
-  $._select=function(selectors,contexts){
-      // Nodes
-      if(typeof selectors!='string'){
-          return selectors;
-      }
-      // New element
-      else if(selectors.match(/^\s*</)){
-          var tag=selectors.match(/^\s*<\s*([a-z]+)/i)[1],
-              table='table',
-              nodeMap={
-                  thead:      table,
-                  tbody:      table,
-                  tfoot:      table,
-                  tr:         'tbody',
-                  th:         'tr',
-                  td:         'tr',
-                  fieldset:   'form',
-                  option:     'select'
-              },
-              root=document.createElement(nodeMap[tag] || 'div'),
-              element,
-              elements=[],
-              i=-1;
-          root.innerHTML=selectors;
-          element=root.firstChild;
-          do{
-              element.nodeType==1 && elements.push(element);
-          }
-          while(element=element.nextSibling)
-          return elements;
-      }
-      // Selectors
-      else{
-          return require('nut')(selectors,contexts);
-      }
-  };
-
-}();
