313982f302e15eecdb5ac4af671c24f8ed0ababc
pyrsmk <dev@dreamysource.fr>
2014-03-27 18:45:59 +0100

    Update ender support and remove array context handling

---
+++ unit/js/ender.js
@@ -0,0 +1,529 @@
+/*!
+  * =============================================================
+  * Ender: open module JavaScript framework (https://enderjs.com)
+  * Build: ender build domready nut
+  * Packages: ender-core@2.0.0 ender-commonjs@1.0.7 domready@1.0.4 nut@0.4.0
+  * =============================================================
+  */
+
+(function () {
+
+  /*!
+    * Ender: open module JavaScript framework (client-lib)
+    * http://enderjs.com
+    * License MIT
+    */
+  
+  /**
+   * @constructor
+   * @param  {*=}      item      selector|node|collection|callback|anything
+   * @param  {Object=} root      node(s) from which to base selector queries
+   */
+  function Ender(item, root) {
+    var i
+    this.length = 0 // Ensure that instance owns length
+  
+    if (typeof item == 'string')
+      // start with strings so the result parlays into the other checks
+      // the .selector prop only applies to strings
+      item = ender._select(this['selector'] = item, root)
+  
+    if (null == item) return this // Do not wrap null|undefined
+  
+    if (typeof item == 'function') ender._closure(item, root)
+  
+    // DOM node | scalar | not array-like
+    else if (typeof item != 'object' || item.nodeType || (i = item.length) !== +i || item == item.window)
+      this[this.length++] = item
+  
+    // array-like - bitwise ensures integer length
+    else for (this.length = i = (i > 0 ? ~~i : 0); i--;)
+      this[i] = item[i]
+  }
+  
+  /**
+   * @param  {*=}      item   selector|node|collection|callback|anything
+   * @param  {Object=} root   node(s) from which to base selector queries
+   * @return {Ender}
+   */
+  function ender(item, root) {
+    return new Ender(item, root)
+  }
+  
+  
+  /**
+   * @expose
+   * sync the prototypes for jQuery compatibility
+   */
+  ender.fn = ender.prototype = Ender.prototype
+  
+  /**
+   * @enum {number}  protects local symbols from being overwritten
+   */
+  ender._reserved = {
+    reserved: 1,
+    ender: 1,
+    expose: 1,
+    noConflict: 1,
+    fn: 1
+  }
+  
+  /**
+   * @expose
+   * handy reference to self
+   */
+  Ender.prototype.$ = ender
+  
+  /**
+   * @expose
+   * make webkit dev tools pretty-print ender instances like arrays
+   */
+  Ender.prototype.splice = function () { throw new Error('Not implemented') }
+  
+  /**
+   * @expose
+   * @param   {function(*, number, Ender)}  fn
+   * @param   {object=}                     scope
+   * @return  {Ender}
+   */
+  Ender.prototype.forEach = function (fn, scope) {
+    var i, l
+    // opt out of native forEach so we can intentionally call our own scope
+    // defaulting to the current item and be able to return self
+    for (i = 0, l = this.length; i < l; ++i) i in this && fn.call(scope || this[i], this[i], i, this)
+    // return self for chaining
+    return this
+  }
+  
+  /**
+   * @expose
+   * @param {object|function} o
+   * @param {boolean=}        chain
+   */
+  ender.ender = function (o, chain) {
+    var o2 = chain ? Ender.prototype : ender
+    for (var k in o) !(k in ender._reserved) && (o2[k] = o[k])
+    return o2
+  }
+  
+  /**
+   * @expose
+   * @param {string}  s
+   * @param {Node=}   r
+   */
+  ender._select = function (s, r) {
+    return s ? (r || document).querySelectorAll(s) : []
+  }
+  
+  /**
+   * @expose
+   * @param {function} fn
+   */
+  ender._closure = function (fn) {
+    fn.call(document, ender)
+  }
+  
+  if (typeof module !== 'undefined' && module['exports']) module['exports'] = ender
+  var $ = ender
+  
+  /**
+   * @expose
+   * @param {string} name
+   * @param {*}      value
+   */
+  ender.expose = function (name, value) {
+    ender.expose.old[name] = window[name]
+    window[name] = value
+  }
+  
+  /**
+   * @expose
+   */
+  ender.expose.old = {}
+  
+  /**
+   * @expose
+   * @param {boolean} all   restore only $ or all ender globals
+   */
+  ender.noConflict = function (all) {
+    window['$'] = ender.expose.old['$']
+    if (all) for (var k in ender.expose.old) window[k] = ender.expose.old[k]
+    return this
+  }
+  
+  ender.expose('$', ender)
+  ender.expose('ender', ender); // uglify needs this semi-colon between concating files
+  
+  /*!
+    * Ender: open module JavaScript framework (module-lib)
+    * http://enderjs.com
+    * License MIT
+    */
+  
+  var global = this
+  
+  /**
+   * @param  {string}  id   module id to load
+   * @return {object}
+   */
+  function require(id) {
+    if ('$' + id in require._cache)
+      return require._cache['$' + id]
+    if ('$' + id in require._modules)
+      return (require._cache['$' + id] = require._modules['$' + id]._load())
+    if (id in window)
+      return window[id]
+  
+    throw new Error('Requested module "' + id + '" has not been defined.')
+  }
+  
+  /**
+   * @param  {string}  id       module id to provide to require calls
+   * @param  {object}  exports  the exports object to be returned
+   */
+  function provide(id, exports) {
+    return (require._cache['$' + id] = exports)
+  }
+  
+  /**
+   * @expose
+   * @dict
+   */
+  require._cache = {}
+  
+  /**
+   * @expose
+   * @dict
+   */
+  require._modules = {}
+  
+  /**
+   * @constructor
+   * @param  {string}                                          id   module id for this module
+   * @param  {function(Module, object, function(id), object)}  fn   module definition
+   */
+  function Module(id, fn) {
+    this.id = id
+    this.fn = fn
+    require._modules['$' + id] = this
+  }
+  
+  /**
+   * @expose
+   * @param  {string}  id   module id to load from the local module context
+   * @return {object}
+   */
+  Module.prototype.require = function (id) {
+    var parts, i
+  
+    if (id.charAt(0) == '.') {
+      parts = (this.id.replace(/\/.*?$/, '/') + id.replace(/\.js$/, '')).split('/')
+  
+      while (~(i = parts.indexOf('.')))
+        parts.splice(i, 1)
+  
+      while ((i = parts.lastIndexOf('..')) > 0)
+        parts.splice(i - 1, 2)
+  
+      id = parts.join('/')
+    }
+  
+    return require(id)
+  }
+  
+  /**
+   * @expose
+   * @return {object}
+   */
+  Module.prototype._load = function () {
+    var m = this
+  
+    if (!m._loaded) {
+      m._loaded = true
+  
+      /**
+       * @expose
+       */
+      m.exports = {}
+      m.fn.call(global, m, m.exports, function (id) { return m.require(id) }, global)
+    }
+  
+    return m.exports
+  }
+  
+  /**
+   * @expose
+   * @param  {string}                     id        main module id
+   * @param  {Object.<string, function>}  modules   mapping of module ids to definitions
+   * @param  {string}                     main      the id of the main module
+   */
+  Module.createPackage = function (id, modules, main) {
+    var path, m
+  
+    for (path in modules) {
+      new Module(id + '/' + path, modules[path])
+      if (m = path.match(/^(.+)\/index$/)) new Module(id + '/' + m[1], modules[path])
+    }
+  
+    if (main) require._modules['$' + id] = require._modules['$' + id + '/' + main]
+  }
+  
+  if (ender && ender.expose) {
+    /*global global,require,provide,Module */
+    ender.expose('global', global)
+    ender.expose('require', require)
+    ender.expose('provide', provide)
+    ender.expose('Module', Module)
+  }
+  
+  Module.createPackage('domready', {
+    'ready': function (module, exports, require, global) {
+      /*!
+        * domready (c) Dustin Diaz 2014 - License MIT
+        */
+      !function (name, definition) {
+      
+        if (typeof module != 'undefined') module.exports = definition()
+        else if (typeof define == 'function' && typeof define.amd == 'object') define(definition)
+        else this[name] = definition()
+      
+      }('domready', function () {
+      
+        var fns = [], listener
+          , doc = document
+          , domContentLoaded = 'DOMContentLoaded'
+          , loaded = /^loaded|^i|^c/.test(doc.readyState)
+      
+        if (!loaded)
+        doc.addEventListener(domContentLoaded, listener = function () {
+          doc.removeEventListener(domContentLoaded, listener)
+          loaded = 1
+          while (listener = fns.shift()) listener()
+        })
+      
+        return function (fn) {
+          loaded ? fn() : fns.push(fn)
+        }
+      
+      });
+      
+    },
+    'src\ender': function (module, exports, require, global) {
+      !function ($) {
+        var ready = require('domready')
+        $.ender({domReady: ready})
+        $.ender({
+          ready: function (f) {
+            ready(f)
+            return this
+          }
+        }, true)
+      }(ender);
+    }
+  }, 'ready');
+
+  Module.createPackage('nut', {
+    'src\nut': function (module, exports, require, global) {
+      /*      
+      	nut, the concise CSS selector engine      
+            
+      	Author      
+      		Aurélien Delogu (dev@dreamysource.fr)      
+      */      
+            
+      (function(name,context,definition){      
+      	if(typeof module!='undefined' && module.exports){      
+      		module.exports=definition();      
+      	}      
+      	else if(typeof define=='function' && define.amd){      
+      		define(definition);      
+      	}      
+      	else{      
+      		context[name]=definition();      
+      	}      
+      }('nut',this,function(){      
+            
+      	/*      
+      		Get id node      
+            
+      		Parameters      
+      			String selector : one selector      
+      			Object context  : one context      
+            
+      		Return      
+      			Array           : nodes      
+      	*/      
+      	var getNodesFromIdSelector=function(selector,context){      
+      		var node=document.getElementById(selector);      
+      		if(!node){      
+      			return [];      
+      		}      
+      		else{      
+      			return [node];      
+      		}      
+      	},      
+            
+      	/*      
+      		Get nodes corresponding to one class name (for IE<9)      
+            
+      		Parameters      
+      			String selector : one selector      
+      			Object context  : one context      
+            
+      		Return      
+      			Array           : nodes      
+      	*/      
+      	getNodesByClassName=function(name,context){      
+      		// Init vars      
+      		var node=context.firstChild,      
+      			nodes=[],      
+      			elements;      
+      		// Browse children      
+      		if(node){      
+      			do{      
+      				if(node.nodeType==1){      
+      					// Match the class      
+      					if(node.className && node.className.match('\\b'+name+'\\b')){      
+      						nodes.push(node);      
+      					}      
+      					// Get nodes from node's children      
+      					if((elements=getNodesByClassName(name,node)).length){      
+      						nodes=nodes.concat(elements);      
+      					}      
+      				}      
+      			}      
+      			while(node=node.nextSibling);      
+      		}      
+      		return nodes;      
+      	},      
+            
+      	/*      
+      		Get nodes from a class selector      
+            
+      		Parameters      
+      			String selector : one selector      
+      			Object context  : one context      
+            
+      		Return      
+      			Array           : nodes      
+      	*/      
+      	getNodesFromClassSelector=function(selector,context){      
+      		if(context.getElementsByClassName){      
+      			return context.getElementsByClassName(selector);      
+      		}      
+      		else{      
+      			return getNodesByClassName(selector,context);      
+      		}      
+      	},      
+            
+      	/*      
+      		Get nodes from a tag selector      
+            
+      		Parameters      
+      			String selector : one selector      
+      			Object context  : one context      
+            
+      		Return      
+      			Array           : nodes      
+      	*/      
+      	getNodesFromTagSelector=function(selector,context){      
+      		return context.getElementsByTagName(selector);      
+      	};      
+            
+      	/*      
+      		Select DOM nodes      
+            
+      		Parameters      
+      			String selectors : CSS selectors      
+      			Object context   : contextual node      
+            
+      		Return      
+      			Array            : found nodes      
+      	*/      
+      	return function(selectors,context){      
+      		// Format      
+      		if(!context){      
+      			context=document;      
+      		}      
+      		// Init vars      
+      		var local_contexts,      
+      			future_local_contexts,      
+      			selector,      
+      			elements,      
+      			nodes=[],      
+      			j,k,l,m,n,o,      
+      			getNodesFromSelector;      
+      		// Prepare selectors      
+      		selectors=selectors.split(',');      
+      		n=-1;      
+      		while(selector=selectors[++n]){      
+      			selectors[n]=selector.split(/\s+/);      
+      		}      
+      		// Evaluate selectors for each global context      
+      		j=selectors.length;      
+      		while(j){      
+      			// Init local context      
+      			local_contexts=[context];      
+      			// Evaluate selectors      
+      			k=-1;      
+      			l=selectors[--j].length;      
+      			while(++k<l){      
+      				// Drop empty selectors      
+      				if(selector=selectors[j][k]){      
+      					// Id      
+      					if(selector.charAt(0)=='#'){      
+      						selector=selector.substr(1);      
+      						getNodesFromSelector=getNodesFromIdSelector;      
+      					}      
+      					// Class      
+      					else if(selector.charAt(0)=='.'){      
+      						selector=selector.substr(1);      
+      						getNodesFromSelector=getNodesFromClassSelector;      
+      					}      
+      					// Tag      
+      					else{      
+      						getNodesFromSelector=getNodesFromTagSelector;      
+      					}      
+      					// Evaluate current selector for each local context      
+      					future_local_contexts=[];      
+      					m=-1;      
+      					while(local_contexts[++m]){      
+      						elements=getNodesFromSelector(selector,local_contexts[m]);      
+      						n=-1;      
+      						o=elements.length;      
+      						while(++n<o){      
+      							future_local_contexts.push(elements[n]);      
+      						}      
+      					}      
+      					// Set new local contexts      
+      					local_contexts=future_local_contexts;      
+      				}      
+      			}      
+      			// Append new nodes      
+      			nodes=nodes.concat(local_contexts);      
+      		}      
+      		return nodes;      
+      	};      
+            
+      }));      
+      
+    },
+    'src\ender': function (module, exports, require, global) {
+      $._select=function(selector,context){
+      	if(typeof selector=='object'){
+      		return selector;
+      	}
+      	else{
+      		return require('nut')(selector,context);
+      	}
+      };
+    }
+  }, 'src\nut');
+
+  require('domready');
+  require('domready/src\ender');
+  require('nut');
+  require('nut/src\ender');
+
+}.call(window));
+//# sourceMappingURL=ender.js.map
