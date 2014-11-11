63552f2dbc54fbc4dc4682506744ccbaa79b7e72
pyrsmk <dev@dreamysource.fr>
2014-11-11 12:14:45 +0100

    Update AMD declaration

--- src/nut.js
+++ src/nut.js
@@ -7,13 +7,13 @@
 
 ;(function(context,name,definition){
 	if(typeof module!='undefined' && module.exports){
-		module.exports=definition();
+		module.exports=definition;
 	}
 	else if(typeof define=='function' && define.amd){
 		define(definition);
 	}
 	else{
-		context[name]=definition();
+		context[name]=definition;
 	}
 }(this,'nut',function(){
 
@@ -179,4 +179,4 @@
 		return nodes;
 	};
 
-}));
+}()));
